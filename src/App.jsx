d03e5928f51282454778d30554fb8cb363337836
anjali29singh <anjalisingh930260@gmail.com>
2023-12-16 11:43:49 +0530

    multiple language implementation

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,12 @@
 import "./App.css";
 import Header from "./assets/components/Header";
 import SourceCode from "./assets/components/SourceCode";
-import TransitCode from "./assets/components/TransitCode";
 
 function App() {
   return (
     <>
       <Header />
-      <div className="main">
-        <SourceCode />
-        <TransitCode />
-      </div>
+      <SourceCode />
     </>
   );
 }
--- src/assets/components/SourceCode.jsx
+++ src/assets/components/SourceCode.jsx
@@ -1,5 +1,9 @@
 import { useState } from "react";
 import OpenAI from "openai";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import { IconContext } from "react-icons";
+import { FaArrowCircleRight } from "react-icons/fa";
 
 const openai = new OpenAI({
   apiKey: import.meta.env.VITE_OPENAI_API_KEY,
@@ -12,11 +16,11 @@ async function getCompletion(req) {
     messages: [
       {
         role: "system",
-        content: "convert given code into python code",
+        content: `convert given code into ${req.selectedLang} code`,
       },
       {
         role: "user",
-        content: req,
+        content: req.enteredCode,
       },
     ],
     model: "gpt-3.5-turbo-16k-0613",
@@ -28,31 +32,65 @@ async function getCompletion(req) {
 export default function SourceCode() {
   const [enteredCode, setEnteredCode] = useState("");
   const [data, setData] = useState("");
+  const [selectedLang, setSelectedLang] = useState("python");
+
+  const content = { enteredCode, selectedLang };
+  //handle text area change
 
   const handleChange = (e) => {
-    // if(e.target.value==='' || !e.target.value){
-
-    // }
     setEnteredCode(e.target.value);
   };
 
+  //handle button click
+
   const handleClick = async (e) => {
     e.preventDefault();
-    const x = await getCompletion(enteredCode);
+    if (enteredCode === "") {
+      toast("Please enter code");
+      return;
+    }
+    toast.info("Please wait while we convert your code");
+    const x = await getCompletion(content);
     setData(x.content);
-    console.log(data);
+    console.log(x.content);
+    toast.success("Code converted successfully");
+    setData(x.content);
+  };
+
+  const onChangeLang = async (e) => {
+    setSelectedLang(e.target.value);
+
+    const y = await getCompletion(content);
+    setData(y.content);
   };
 
   return (
-    <div className="inputBox">
-      <textarea
-        className="text-area"
-        placeholder="enter code here"
-        onChange={handleChange}
-      ></textarea>
-      <button title="Click Me" className="btn" onClick={handleClick}>
-        Click Me
-      </button>
+    <div className="main">
+      <div className="inputBox">
+        <ToastContainer />
+        <textarea
+          className="text-area"
+          placeholder="enter code here"
+          onChange={handleChange}
+        ></textarea>
+        <IconContext.Provider
+          value={{ color: "green", className: "submitBtn", size: "2em" }}
+        >
+          <FaArrowCircleRight onClick={handleClick} />
+        </IconContext.Provider>
+      </div>
+      <div className="outputBox">
+        <select
+          className="select-lang"
+          style={{ fontSize: "medium" }}
+          onChange={onChangeLang}
+        >
+          <option value="python">Python</option>
+          <option value="javascript">Javascript</option>
+          <option value="c++">C++</option>
+          <option value="java">Java</option>
+        </select>
+      </div>
     </div>
   );
 }
